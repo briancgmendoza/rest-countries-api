929387717a16c2170ed714825e21516708355ff8
Brian Christopher Mendoza <78596685+briancgmendoza@users.noreply.github.com>
2022-05-28 22:44:02 +0800

    Adding react-router-dom and themeContextProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeContextProvider } from "./store/theme-context";
+
 import App from "./app";
 import "./styles/index.module.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+  </BrowserRouter>
+);
